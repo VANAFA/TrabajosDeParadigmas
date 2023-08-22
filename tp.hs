fa8c1dc2e386acc2a0d0faacbc4503b57fe8a1f7
VANAFA <vfadel@udesa.edu.ar>
2023-08-22 17:30:23 -0300

    Update tp.hs
    optimización difP

--- tp.hs
+++ tp.hs
@@ -17,7 +17,7 @@ newP :: Int -> Int -> Point
 newP x y = Poi x y
 
 difP :: Point -> Point -> Float  -- distancia absoluta
-difP (Poi x1 y1) (Poi x2 y2) = sqrt ((fromIntegral (x1 - x2) ^ 2) + (fromIntegral (y1 - y2) ^ 2))
+difP (Poi x1 y1) (Poi x2 y2) = sqrt $ fromIntegral $ (x1 - x2)^2 + (y1 - y2)^2
 
 test = [ newP 1 2 == Poi 1 2,
          difP (newP 1 2) (newP 0 0) == sqrt 5,
