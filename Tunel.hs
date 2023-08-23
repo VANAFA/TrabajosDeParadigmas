efb5c9bd4a4f40409f106b630c7ada7637b1c956
VANAFA <vfadel@udesa.edu.ar>
2023-08-23 15:26:56 -0300

    All done up to Tunel.hs
    Tunnel WIP
    test file improvement

--- Tunel.hs
+++ Tunel.hs
@@ -10,8 +10,8 @@ newT :: [Link] -> Tunel
 newT = Tun
 
 connectsT :: City -> City -> Tunel -> Bool -- inidca si este tunel conceta estas dos ciudades distintas
-connectsT city1 city2 (Tun link c1 c2) | ((city1 == c1) && (city2 == c2)) || ((city1 == c2) && (city2 == c1)) = True
-                                       | otherwise = False
+connectsT city1 city2 (Tun []) | ((city1 == c1) && (city2 == c2)) || ((city1 == c2) && (city2 == c1)) = True
+                                          | otherwise = False
 
 -- por que connectsT me dice que si el tunel atraviesa las 2 ciudades, y la usesT si atraviesa el link?
 -- el tunel solo toma un link como input, no se donde existe que un tunel que tome el link x no pase por el link x
@@ -22,3 +22,4 @@ usesT link1 (Tun link2) | link1 == link2 = True
                         | otherwise = False
 
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
+delayT tunel = 12.2 --temp
--- test.hs
+++ test.hs
@@ -1,22 +1,25 @@
-import Point 
-import City
-import Quality
-import Link
+import Tunel
 
 point1 = newP 1 2
 point2 = newP 0 0
+point3 = newP 3 4
+point4 = newP 5 4
 city1 = newC "Vice City" point1
 city2 = newC "Liberty City" point2
-qua = newQ "deAaB" 3 0.5
-ab = newL city1 city2 qua
+city3 = newC "Los Santos" point3
+city4 = newC "San Andres" point4
+qua = newQ "qua1" 3 0.5
+ViceToLiberty = newL city1 city2 qua
+LibertyToSantos = newL city2 city3 qua
 
 test = [difP (newP 1 2) (newP 0 0) == sqrt 5,
         nameC city1 == "Vice City",
         distanceC city1 city2 == sqrt 5,
         capacityQ qua == 3,
         delayQ qua == 0.5,
-        connectsL city1 ab,
-        linksL city2 city1 ab,
-        capacityL ab == 3,
-        delayL ab == 0.5,
+        connectsL city1 ViceToLiberty,
+        linksL city3 city2 LibertyToSantos,
+        capacityL ViceToLiberty == 3,
+        delayL ViceToLiberty == 0.5,
+
         True]
