5dd877411547ccd720c9b071721324bf03d1b63a
VANAFA <vfadel@udesa.edu.ar>
2023-08-22 22:34:27 -0300

    test improvement

--- test.hs
+++ test.hs
@@ -3,10 +3,6 @@ import City
 import Quality
 import Link
 
-data Point = Poi Int Int deriving (Eq, Show)
-
-
-
 point1 = newP 1 2
 point2 = newP 0 0
 city1 = newC "Vice City" point1
@@ -15,8 +11,8 @@ qua = newQ "deAaB" 3 0.5
 ab = newL city1 city2 qua
 
 test = [ newP 1 2 == 1 2,
-         difP point1 point2 == sqrt 5,
-         city1 == Cit "Vice City" (newP 1 2),
+         difP (newP 1 2) (newP 0 0) == sqrt 5,
+         newC "Vice City" point1 == Cit "Vice City" (newP 1 2),
          nameC city1 == "Vice City",
          distanceC city1 city2 == sqrt 5,
          qua == Qua "deAaB" 3 0.5,
